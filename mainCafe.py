4c738a2503e8ff4f896a2d55a9a8fa68f3269fc1
alaaibr25 <94000702+alaaibr25@users.noreply.github.com>
2025-07-27 20:03:59 +0300

    Initial Commit

---
+++ mainCafe.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, validators
+from flask_bootstrap import Bootstrap4
+
+
+app = Flask(__name__)
+app.secret_key = "ndwkcns8uh3jkqwnms"
+
+
+
+@app.route('/')
+def main_page():
+    return render_template('index.html')
+
+@app.route('/cafe')
+def all_cafes():
+    return render_template("cafes.html")
+
+
+@app.route('/add')
+def add_cafe():
+    return render_template("add.html")
+
+
+
+
+
+
+
+
+
+
+app.run(debug=True)
